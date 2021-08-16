e95e816c334015b6213b7b18da1f7a771f8d38b8
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-08-16 09:20:48 +0300

    Идеальная версия от преподавателя

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,18 +2,16 @@ function compareArrays(arr1, arr2) {
 
 	// Ваш код
 
-	let result = arr1.every((item, idx) => arr1.length === arr2.length && arr1[idx] === arr2[idx]);
-
-	return result;
+	return arr1.length === arr2.length && arr1.every((item, idx) => arr1[idx] === arr2[idx]);
 };
 
 
 
 
 function advancedFilter(arr) {
-	let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
-	// Ваш код
 
-	return resultArr; // array
+	return arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+
+
 }
