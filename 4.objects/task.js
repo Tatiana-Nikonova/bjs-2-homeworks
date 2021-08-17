47afb68807705a67d4ec7be3eca0b5b31e9aa922
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-08-17 15:28:22 +0300

    Первичная версия

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,65 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+	// Ваш код
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+	//ваш код
+	this.subject = subjectName;
+};
 
 // ваш код для остальных методов
+
+
+Student.prototype.addMark = function (...mark) {
+	//ваш код
+
+	if (this.marks === undefined) {
+		// добавить первую оценку 
+		this.marks = mark;
+
+	} else {
+		// добавить вторую и последующие оценки в массив
+		this.marks.push(...mark);
+	};
+	let arr = this.marks;
+	return arr;
+};
+
+
+Student.prototype.getAverage = function (arr) {
+	//ваш код
+	let i = 0;
+	let sum = arr.forEach((item) => i += item);
+
+	let avg = sum / arr.length;
+	return avg = Number(avg.toFixed(2));
+};
+
+Student.prototype.excludeStudent = function (reason) {
+	//ваш код
+	//getAverage(arr);
+
+	if (avg < 3) {
+		delete this.subject;
+		delete this.marks;
+
+		this.excluded = reason;
+	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
