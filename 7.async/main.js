897f63af95a2bce712902d56517da99f03d64b20
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-09-17 15:47:31 +0300

    Вся домашка с одной ошибкой

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,20 @@
 // тут вы можете вызывать функции из task.js
+let phoneAlarm = new AlarmClock();
+
+phoneAlarm.addClock('09:00', () => console.log('Пора вставать'), 1);
+phoneAlarm.addClock('09:01', () => { console.log('Давай, вставай уже!'); phoneAlarm.removeClock(2) }, 2);
+phoneAlarm.addClock('09:01', () => console.log('Иди умываться!'));
+
+phoneAlarm.addClock('09:02', () => {
+	console.log('Вставай, а то проспишь!');
+	phoneAlarm.clearAlarms();
+	phoneAlarm.printAlarms();
+}, 3);
+
+phoneAlarm.addClock('09:05', () => console.log('Вставай, а то проспишь!'), 1);
+
+phoneAlarm.printAlarms();
+
+phoneAlarm.start();
+
+
