323dba8a52798ce74ea55cf864602ddff96527be
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-09-21 17:05:02 +0300

    Ошибка в первой -1, ошибка в третьей счет 1 раз

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,77 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+	// Ваш код
+	let cache = [];
+
+	function wrapper(...args) {
+
+		let hash = args.join(',');
+
+		const idx = cache.findIndex((item) => item in cache);
+
+		if (idx !== -1) {
+			console.log("Из кэша: " + cache[hash]);
+			return "Из кэша: " + cache[hash];
+		} else {
+			let result = func(...args);
+			cache.push(args);
+
+			if (cache.length > 5) {
+				cache.shift();
+			};
+			console.log("Вычисляем: " + result);
+			return "Вычисляем: " + result;
+		};
+	};
+
+	return wrapper;
+};
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+
+
+function debounceDecoratorNew(func, ms) {
+	// Ваш код
+	let isThrottled = false;
+
+	return function wrapper(...args) {
+
+		if (isThrottled) return;
+
+		func.apply(this, args);
+		isThrottled = true;
+
+		setTimeout(() => {
+			isThrottled = false;
+			func.apply(this, args);
+		}, ms);
+	};
+};
+
+
+
+function debounceDecorator2(func, ms) {
+	// Ваш код
+	let isThrottled = false;
+
+
+	return function wrapper(...args) {
+		wrapper.count = 0;
+		if (isThrottled) {
+			return;
+		} else {
+			func.apply(this, args);
+			wrapper.count++;
+			isThrottled = true;
+
+			setTimeout(() => {
+				isThrottled = false;
+				func.apply(this, args);
+			}, ms);
+		};
+
+		console.log(wrapper.count);
+		return wrapper;
+	};
+};
+
